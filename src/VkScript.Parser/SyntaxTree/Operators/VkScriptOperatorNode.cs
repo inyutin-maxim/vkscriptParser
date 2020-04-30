fdccba69274d9d9983378e5a2ab695c83a8b456a
Александр <41797593+satyrcon@users.noreply.github.com>
2020-04-30 17:18:01 +0300

    VkScriptOperatorNode (#56)
    Co-authored-by: satyrcon <satyrcon@users.noreply.github.com>

---
+++ src/VkScript.Parser/SyntaxTree/Operators/VkScriptOperatorNode.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using VkScript.Parser.Abstractions;
+
+namespace VkScript.Parser.SyntaxTree.Operators
+{
+	/// <summary>
+	/// Базовая нода для всех операторов
+	/// </summary>
+	internal abstract class VkScriptOperatorNode : VkScriptSyntaxNode
+	{
+	#region Operator basics
+
+		/// <summary>
+		/// Текстовое представление оператора
+		/// </summary>
+		protected abstract string OperatorRepresentation { get; }
+
+		/// <summary>
+		/// The name of the method that C# compiler uses for method overloading.
+		/// </summary>
+		protected virtual string OverloadedMethodName => null;
+
+	#endregion
+
+	}
+}
